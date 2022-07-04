99f20574397ae0e1cfa6a4144a8141bcd3c67771
Leo <leonelsoto17@gmail.com>
2022-07-04 13:45:04 -0500

    tweaked publicPath to link to dist/

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: "/restaurant-page/"
+        publicPath: "/restaurant-page/dist"
     },
     module: {
         rules: [
